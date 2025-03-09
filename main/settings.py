b6a29e3ed5ead983fb82e24a5f42db601ea66f69
Peka <vanlalpeka88@gmail.com>
2025-03-10 00:51:39 +0100

    set to IST

--- main/settings.py
+++ main/settings.py
@@ -140,11 +140,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
+TIME_ZONE = 'Asia/Kolkata'  # Set to IST (Indian Standard Time)
+USE_I18N = True  # Enable internationalization
+USE_L10N = True  # Enable localization
+USE_TZ = True    # Enable timezone support (recommended)
 
 
 # Static files (CSS, JavaScript, Images)
