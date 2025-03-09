413a74f8c99cbc01053e2fd697362f8c6ad781d2
Peka <vanlalpeka88@gmail.com>
2025-03-10 00:16:23 +0100

    conditional url import

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from debug_toolbar.toolbar import debug_toolbar_urls
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
