9cfd7fa0486a319424b48241622e6f09ff52cff4
Peka <vanlalpeka88@gmail.com>
2025-03-09 13:47:13 +0100

    init sort

--- app/ajax_datatable_views.py
+++ app/ajax_datatable_views.py
@@ -7,7 +7,7 @@ class NoteAjaxDatatableView(AjaxDatatableView):
 
     model = Note
     title = 'Note'
-    # initial_order = [["id", "asc"], ]
+    initial_order = [["date", "dsc"], ]
     length_menu = [[10, 20, 50, 100, -1], [10, 20, 50, 100, 'all']]
     search_values_separator = '+'
     show_date_filters = True
@@ -16,9 +16,9 @@ class NoteAjaxDatatableView(AjaxDatatableView):
     column_defs = [
         # AjaxDatatableView.render_row_tools_column_def(),
         {'name': 'date' },
+        {'name': 'subject', },
         {'name': 'concerned_department', 'title':'Department', 'foreign_field': 'concerned_department__name',  'choices': True, 'autofilter': True,},
         {'name': 'type', 'foreign_field': 'type__name',  'choices': True, 'autofilter': True,},
-        {'name': 'subject', },
         {'name': 'file_number', 'title': 'File No.'},
         {'name': 'comment', 'visible': True, },
     ]
